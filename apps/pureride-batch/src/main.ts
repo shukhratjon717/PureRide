1235af504664c4c323caaa74c433a290cc45ced1
Shukhratjon <shukhratjon0717@gmail.com>
2024-07-03 19:22:35 +0900

    feat: integration of env variables

--- apps/pureride-batch/src/main.ts
+++ apps/pureride-batch/src/main.ts
@@ -3,6 +3,6 @@ import { PurerideBatchModule } from './pureride-batch.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(PurerideBatchModule);
-  await app.listen(3000);
+  await app.listen(process.env.PORT_BATCH ?? 5000 );
 }
 bootstrap();
--- apps/pureride-batch/src/pureride-batch.module.ts
+++ apps/pureride-batch/src/pureride-batch.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PurerideBatchController } from './pureride-batch.controller';
 import { PurerideBatchService } from './pureride-batch.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot()],
   controllers: [PurerideBatchController],
   providers: [PurerideBatchService],
 })
