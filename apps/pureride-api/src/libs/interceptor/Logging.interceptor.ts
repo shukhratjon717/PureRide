a3d3d08a259eb108701cc048d1eb1437caf8929d
Shukhratjon <shukhratjon0717@gmail.com>
2024-07-04 01:18:05 +0900

    feat: standard integratoin of Logging interceptor

--- apps/pureride-api/src/libs/interceptor/Logging.interceptor.ts
+++ apps/pureride-api/src/libs/interceptor/Logging.interceptor.ts
@@ -1,12 +1,22 @@
-import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from '@nestjs/common';
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
 import { Observable, tap } from 'rxjs';
 
 @Injectable()
 export class Loggingintercepter implements NestInterceptor {
-	intercept(context: ExecutionContext, next: CallHandler<any>): Observable<any> {
-		console.log('Before...');
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler<any>,
+  ): Observable<any> {
+    console.log('Before...');
 
-		const now = Date.now();
-		return next.handle().pipe(tap(() => console.log(`After ... ${Date.now() - now}ms`)));
-	}
+    const now = Date.now();
+    return next
+      .handle()
+      .pipe(tap(() => console.log(`After ... ${Date.now() - now}ms`)));
+  }
 }
