e7b9db872564ffa51a2e50364c9345fb0deb5cdd
Shukhratjon <shukhratjon0717@gmail.com>
2024-07-03 21:40:56 +0900

    feat: create member resolver & service model

---
+++ apps/pureride-api/src/components/member/member.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@nestjs/common';
+
+
+// Main business logic
+@Injectable()
+export class MemberService {
+
+    public async signup():Promise<string > {
+        return 'Signup executed';
+    };
+    public async login():Promise<string > {
+        return 'login executed';
+    }
+    public async updateMember():Promise<string > {
+        return 'updateMember executed';
+    }
+    public async getMember():Promise<string > {
+        return 'getMember executed';
+    }
+
+    
+}
