6b1a99b98586337f21101f7184a640aa6a2b6222
Shukhratjon <shukhratjon0717@gmail.com>
2024-07-04 14:18:21 +0900

    feat: develop single and multi image uploaders and modify upload image configurations

--- apps/pureride-api/src/main.ts
+++ apps/pureride-api/src/main.ts
@@ -1,12 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { graphqlUploadExpress } from 'graphql-upload';
+import * as express from 'express';
 import { Loggingintercepter } from './libs/interceptor/Logging.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new Loggingintercepter());
+  app.enableCors({ origin: true, credentials: true });
+
+  app.use(graphqlUploadExpress({ maxFileSize: 15000000, maxFiles: 10 }));
+  app.use('/uploads', express.static('./uploads'));
+
   await app.listen(process.env.PORT_API ?? 3000);
 }
 bootstrap();
