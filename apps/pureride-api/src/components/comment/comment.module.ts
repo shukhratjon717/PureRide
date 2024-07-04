3a4bdd755af732ba0239329b2ed4c20221375c4f
Shukhratjon <shukhratjon0717@gmail.com>
2024-07-05 00:41:27 +0900

    feat: configure comment & create comment related resolver and service model

--- apps/pureride-api/src/components/comment/comment.module.ts
+++ apps/pureride-api/src/components/comment/comment.module.ts
@@ -1,4 +1,27 @@
 import { Module } from '@nestjs/common';
+import { CommentResolver } from './comment.resolver';
+import { CommentService } from './comment.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import CommentSchema from '../../schemas/Comment.model';
+import { AuthModule } from '../auth/auth.module';
+import { MemberModule } from '../member/member.module';
+import { BoardArticleModule } from '../board-article/board-article.module';
+import { ProductModule } from '../product/product.module';
 
-@Module({})
+@Module({
+	imports: [
+		MongooseModule.forFeature([
+			{
+				name: 'Comment',
+				schema: CommentSchema,
+			},
+		]),
+    AuthModule,
+    MemberModule,
+    ProductModule,
+    BoardArticleModule
+	],
+	providers: [CommentResolver, CommentService],
+
+})
 export class CommentModule {}
