e34f5aeee0a6740231e2f67bf6d4a8e5f3b489c6
Shukhratjon <shukhratjon0717@gmail.com>
2024-07-04 13:43:10 +0900

    feat: develop getAgents GraphQL api

--- apps/pureride-api/src/components/member/member.service.ts
+++ apps/pureride-api/src/components/member/member.service.ts
@@ -1,15 +1,16 @@
 import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model, ObjectId } from 'mongoose';
-import { LoginInput, MemberInput } from '../../libs/dto/member/member.input';
-import { MemberStatus } from '../../libs/enums/member.enum';
-import { Message } from '../../libs/enums/common.enum';
-import { Member } from '../../libs/dto/member/member';
+import { AgentsInquiry, LoginInput, MemberInput } from '../../libs/dto/member/member.input';
+import { MemberStatus, MemberType } from '../../libs/enums/member.enum';
+import { Direction, Message } from '../../libs/enums/common.enum';
+import { Member, Members } from '../../libs/dto/member/member';
 import { AuthService } from '../auth/auth.service';
 import { MemberUpdate } from '../../libs/dto/member/member.update';
 import { T } from '../../libs/types/common';
-import { ViewGroup } from '../../libs/enums/view.enum';
 import { ViewService } from '../view/view.service';
+import { ViewInput } from '../../libs/dto/view/view.input';
+import { ViewGroup } from '../../libs/enums/view.enum';
 
 // Main business logic
 @Injectable()
@@ -90,7 +91,29 @@ export class MemberService {
 		}
 		return targetMember;
 	}
+	public async getAgents(memberId: ObjectId, input: AgentsInquiry): Promise<Members> {
+		const { text } = input.search;
+		const match: T = { memberType: MemberType.AGENT, memberStatus: MemberStatus.ACTIVE };
+		const sort: T = { [input?.sort ?? 'createdAt']: input.direction ?? Direction.DESC };
 
+		if (text) match.memberNick = { $regex: new RegExp(text, 'i') };
+		console.log(match);
+
+		const result = await this.memberModel
+			.aggregate([
+				{ $match: match },
+				{ $sort: sort },
+				{
+					$facet: {
+						list: [{ $skip: (input.page - 1) * input.limit }, { $limit: input.limit }],
+						metaCounter: [{ $count: 'total' }],
+					},
+				},
+			])
+			.exec();
+		if (!result.length) throw new InternalServerErrorException(Message.NO_DATA_FOUND);
+		return result[0];
+	}
 	public async getAllMembersByAdmin(): Promise<string> {
 		return 'getAllMembersByAdmin executed!';
 	}
--- apps/pureride-api/src/libs/dto/member/member.ts
+++ apps/pureride-api/src/libs/dto/member/member.ts
@@ -1,87 +1,98 @@
 import { Field, Int, ObjectType } from '@nestjs/graphql';
-import {
-  MemberAuthType,
-  MemberStatus,
-  MemberType,
-} from '../../enums/member.enum';
+import { MemberAuthType, MemberStatus, MemberType } from '../../enums/member.enum';
 import { ObjectId } from 'mongoose';
 
 @ObjectType()
 export class Member {
-  @Field(() => String)
-  _id: ObjectId;
+	@Field(() => String)
+	_id: ObjectId;
 
-  @Field(() => MemberType)
-  memberType: MemberType;
+	@Field(() => MemberType)
+	memberType: MemberType;
 
-  @Field(() => MemberStatus)
-  memberStatus: MemberStatus;
+	@Field(() => MemberStatus)
+	memberStatus: MemberStatus;
 
-  @Field(() => MemberAuthType)
-  memberAuthType: MemberAuthType;
+	@Field(() => MemberAuthType)
+	memberAuthType: MemberAuthType;
 
-  @Field(() => String)
-  memberPhone: string;
+	@Field(() => String)
+	memberPhone: string;
 
-  @Field(() => String)
-  memberNick: string;
+	@Field(() => String)
+	memberNick: string;
 
-  memberPassword?: string;
+	memberPassword?: string;
 
-  @Field(() => String, { nullable: true })
-  memberFullName?: string;
+	@Field(() => String, { nullable: true })
+	memberFullName?: string;
 
-  @Field(() => String)
-  memberImage: string;
+	@Field(() => String)
+	memberImage: string;
 
-  @Field(() => String, { nullable: true })
-  memberAddress?: string;
+	@Field(() => String, { nullable: true })
+	memberAddress?: string;
 
-  @Field(() => String, { nullable: true })
-  memberDesc?: string;
+	@Field(() => String, { nullable: true })
+	memberDesc?: string;
 
-  @Field(() => Int)
-  memberProperties: number;
+	@Field(() => Int)
+	memberProperties: number;
 
-  @Field(() => Int)
-  memberArticles: number;
+	@Field(() => Int)
+	memberArticles: number;
 
-  @Field(() => Int)
-  memberFollowers: number;
+	@Field(() => Int)
+	memberFollowers: number;
 
-  @Field(() => Int)
-  memberFollowings: number;
+	@Field(() => Int)
+	memberFollowings: number;
 
-  @Field(() => Int)
-  memberPoints: number;
+	@Field(() => Int)
+	memberPoints: number;
 
-  @Field(() => Int)
-  memberLikes: number;
+	@Field(() => Int)
+	memberLikes: number;
 
-  @Field(() => Int)
-  memberViews: number;
+	@Field(() => Int)
+	memberViews: number;
 
-  @Field(() => Int)
-  memberComments: number;
+	@Field(() => Int)
+	memberComments: number;
 
-  @Field(() => Int)
-  memberRank: number;
+	@Field(() => Int)
+	memberRank: number;
 
-  @Field(() => Int)
-  memberWarnings: number;
+	@Field(() => Int)
+	memberWarnings: number;
 
-  @Field(() => Int)
-  memberBlocks: number;
+	@Field(() => Int)
+	memberBlocks: number;
 
-  @Field(() => Date, { nullable: true })
-  deletedAt?: number;
+	@Field(() => Date, { nullable: true })
+	deletedAt?: number;
 
-  @Field(() => Date)
-  createdAt: number;
+	@Field(() => Date)
+	createdAt: number;
 
-  @Field(() => Date)
-  updatedAt: number;
+	@Field(() => Date)
+	updatedAt: number;
 
-  @Field(() => String, { nullable: true })
-  accessToken?: string;
+	@Field(() => String, { nullable: true })
+	accessToken?: string;
+}
+
+@ObjectType()
+export class TotalCounter {
+	@Field(() => Int, { nullable: true })
+    total?: number; 
+}
+
+@ObjectType()
+export class Members {
+	@Field(() => [Member])
+    list: Member[];
+
+	@Field(() => [TotalCounter], { nullable: true })
+	metaCounter?: TotalCounter[];
 }
