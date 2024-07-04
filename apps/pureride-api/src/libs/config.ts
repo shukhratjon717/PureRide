3fe5a1b358b5698a3b1804055f0a2a8836356085
Shukhratjon <shukhratjon0717@gmail.com>
2024-07-05 03:35:05 +0900

    feat: develop subscribe, unsubscribe, getMemberFollowings and getMemberFollowers GraphQL APIs

--- apps/pureride-api/src/libs/config.ts
+++ apps/pureride-api/src/libs/config.ts
@@ -1,40 +1,74 @@
-import { ObjectId } from 'bson';
-import { v4 as uuidv4 } from 'uuid';
-import * as path from 'path';
+import { ObjectId } from "bson";
+import { v4 as uuidv4 } from "uuid";
+import * as path from "path";
 
-export const availableAgentSorts = ['createdAt', 'updatedAt', 'memberLikes', 'memberViews', 'memberRank'];
-export const availableMembersSorts = ['createdAt', 'updatedAt', 'memberLikes', 'memberViews'];
-
-export const availableOptions = ['propertyBarter', 'propertyRent'];
-export const availableProductSorts = [
-	'createdAt',
-	'updatedAt',
-	'productLikes',
-	'productView',
-	'productRank',
-	'productPrice',
+export const availableAgentSorts = [
+  "createdAt",
+  "updatedAt",
+  "memberLikes",
+  "memberViews",
+  "memberRank",
+];
+export const availableMembersSorts = [
+  "createdAt",
+  "updatedAt",
+  "memberLikes",
+  "memberViews",
 ];
 
-export const availableBoardArticles = ['createdAt', 'updatedAt', 'articleLikes', 'articleViews'];
+export const availableOptions = ["propertyBarter", "propertyRent"];
+export const availableProductSorts = [
+  "createdAt",
+  "updatedAt",
+  "productLikes",
+  "productView",
+  "productRank",
+  "productPrice",
+];
 
-export const availableCommentSorts = ['createdAt', 'updatedAt'];
+export const availableBoardArticles = [
+  "createdAt",
+  "updatedAt",
+  "articleLikes",
+  "articleViews",
+];
+
+export const availableCommentSorts = ["createdAt", "updatedAt"];
 /**  IMAGE CONFIGURATION (config.js) **/
 
-export const validMimeTypes = ['image/png', 'image/jpg', 'image/jpeg'];
+export const validMimeTypes = ["image/png", "image/jpg", "image/jpeg"];
 export const getSerialForImage = (filename: string) => {
-	const ext = path.parse(filename).ext;
-	return uuidv4() + ext;
+  const ext = path.parse(filename).ext;
+  return uuidv4() + ext;
 };
 
 export const shapeIntoMongoObjectId = (target: any) => {
-	return typeof target === 'string' ? new ObjectId(target) : target;
+  return typeof target === "string" ? new ObjectId(target) : target;
 };
 
 export const lookupMember = {
-	$lookup: {
-		from: 'members',
-		localField: 'memberId',
-		foreignField: '_id',
-		as: 'memberData',
-	},
+  $lookup: {
+    from: "members",
+    localField: "memberId",
+    foreignField: "_id",
+    as: "memberData",
+  },
+};
+
+export const lookupFollowingData = {
+  $lookup: {
+    from: "members",
+    localField: "followingId",
+    foreignField: "_id",
+    as: "followingData",
+  },
+};
+
+export const lookupFollowerData = {
+  $lookup: {
+    from: "members",
+    localField: "followingId",
+    foreignField: "_id",
+    as: "followerData",
+  },
 };
