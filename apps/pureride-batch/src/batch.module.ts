93b5adfc77b37c417bec482cc4cade7d73817ec5
Shukhratjon <shukhratjon0717@gmail.com>
2024-08-10 00:03:11 +0900

    feat: deploy nestar monorepo project on docker containers

--- apps/pureride-batch/src/batch.module.ts
+++ apps/pureride-batch/src/batch.module.ts
@@ -1,22 +1,22 @@
-import { Module } from "@nestjs/common";
-import { ConfigModule } from "@nestjs/config";
-import { BatchService } from "./batch.service";
-import { DatabaseModule } from "./database/database.module";
-import { ScheduleModule } from "@nestjs/schedule";
-import { MongooseModule } from "@nestjs/mongoose";
-import ProductSchema from "apps/pureride-api/src/schemas/Product.model";
-import MemberSchema from "apps/pureride-api/src/schemas/Member.model";
-import { PurerideBatchController } from "./batch.controller";
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { BatchService } from './batch.service';
+import { DatabaseModule } from './database/database.module';
+import { ScheduleModule } from '@nestjs/schedule';
+import { MongooseModule } from '@nestjs/mongoose';
+import ProductSchema from 'apps/pureride-api/src/schemas/Product.model';
+import MemberSchema from 'apps/pureride-api/src/schemas/Member.model';
+import { PurerideBatchController } from './batch.controller';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot(),
-    DatabaseModule,
-    ScheduleModule.forRoot(),
-    MongooseModule.forFeature([{ name: "Products", schema: ProductSchema }]),
-    MongooseModule.forFeature([{ name: "Member", schema: MemberSchema }]),
-  ],
-  controllers: [PurerideBatchController],
-  providers: [BatchService],
+	imports: [
+		ConfigModule.forRoot(),
+		DatabaseModule,
+		ScheduleModule.forRoot(),
+		MongooseModule.forFeature([{ name: 'Product', schema: ProductSchema }]),
+		MongooseModule.forFeature([{ name: 'Member', schema: MemberSchema }]),
+	],
+	controllers: [PurerideBatchController],
+	providers: [BatchService],
 })
 export class BatchModule {}
