ddc573250ca324b4bc0224ce1ce2abee5bd85550
Shukhratjon <shukhratjon0717@gmail.com>
2026-01-01 13:33:07 +0900

    fix: modify enums

--- apps/pureride-api/src/libs/enums/product.enum.ts
+++ apps/pureride-api/src/libs/enums/product.enum.ts
@@ -33,18 +33,24 @@ registerEnumType(ProductLocation, {
 	name: 'ProductLocation',
 });
 
+// Backend
 export enum ProductEngineSize {
-	BASE = "BASE",
+	BASE = 'BASE',
 	LIGHTWEIGHT = 'LIGHTWEIGHT',
 	ENTRYLEVEL = 'ENTRYLEVEL',
 	INTERMEDIATE = 'INTERMEDIATE',
-	ADVANCED="ADVANCED",
+	ADVANCED = 'ADVANCED',
 	HEAVY = 'HEAVY',
 }
-registerEnumType(ProductEngineSize, {
-	name: 'ProductEngineSize',
-});
-
+registerEnumType(ProductEngineSize, { name: 'ProductEngineSize' });
+export const ProductEngineSizeCC: Record<ProductEngineSize, number> = {
+	[ProductEngineSize.BASE]: 50,
+	[ProductEngineSize.LIGHTWEIGHT]: 125,
+	[ProductEngineSize.ENTRYLEVEL]: 250,
+	[ProductEngineSize.INTERMEDIATE]: 500,
+	[ProductEngineSize.ADVANCED]: 750,
+	[ProductEngineSize.HEAVY]: 1000,
+};
 export enum ProductFuelType {
 	GASOLINE = 'GASOLINE',
 	ELECTRICITY = 'ELECTRICITY',
